72ef87961a20348e5eb59eb1169bfbd3d77690da
abdul jeelan <abul.jeelan@timwe.com>
2026-05-05 06:03:58 +0400

    first commit

---
+++ day_1/index.js
@@ -0,0 +1,18 @@
+console.log(`Hello`);
+
+console.log(`I love Anda`);
+
+// window.alert(`Hello Ande`);
+
+// window.alert("Hello Pizza");
+
+// Single line comment
+
+/*
+Multi
+line
+comments
+*/
+
+document.getElementById("myH1").textContent = "Hello World,This is h1 Tag";
+document.getElementById("myP").textContent = "Hello Ande, This is p Tag";
