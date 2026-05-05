09ff0e26e4d98a504fac093a51a2f2429c267d5a
abdul jeelan <abul.jeelan@timwe.com>
2026-05-05 06:29:14 +0400

    day 2

---
+++ day_2/index.js
@@ -0,0 +1,19 @@
+let name = "Abdul Jeelan";
+let age = 28;
+let professional = true;
+let salary = 1.5;
+/*
+There are total of 3 datatypes in javascript
+Number
+String
+Boolean
+
+*/
+
+console.log(typeof salary); // to get the data type
+
+document.getElementById("p1").textContent = `My Name is ${name}`;
+document.getElementById("p2").textContent = ` My age is ${age}`;
+document.getElementById("p3").textContent =
+  `I'm a Working professional ${professional}`;
+document.getElementById("p4").textContent = `My Salary ${salary}`;
