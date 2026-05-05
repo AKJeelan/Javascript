8cd1039dbb622f08bba88bd92c16ccc80e60d60d
abdul jeelan <abul.jeelan@timwe.com>
2026-05-05 06:48:21 +0400

    update

---
+++ arithmatic_operators/index.js
@@ -0,0 +1,23 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                                         operators (+ - * /)
+//                                         ex. 11 = x + 5;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let students = 30;
+
+console.log(students + 30);
+console.log(students - 30);
+console.log(students * 30);
+console.log(students / 30);
+console.log(students ** 2);
+console.log(students++);
+console.log(students--);
+console.log(++students);
+console.log(--students);
